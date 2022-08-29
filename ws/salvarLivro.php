6654e5c83dbe131ce9e5c33f965ae7665244d454
JQSP <90280224+JQSP@users.noreply.github.com>
2022-08-29 15:04:07 -0300

    SalvarLivro

---
+++ ws/salvarLivro.php
@@ -0,0 +1,9 @@
+<?php
+require_once "../model/Conexao.php";
+
+$nomeLivro = $_GET["txt_livro"];
+$sql = "INSERT INTO public.book
+        (nome, paginas, autor)
+        VALUES('$nomeLivro', 100, 'Desconhecido');";
+
+Conexao::exec($sql);
